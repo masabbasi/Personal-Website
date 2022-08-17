ceb0f7e3d5e8f639b73769e0313b6c7c5265ef76
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-08-17 15:22:17 +0430

    Set Resume Content - Style And JS

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@ mobileMenu.addEventListener("click",function(){
 resumeListItems.forEach(resumeListItem=>{
 	resumeListItem.addEventListener("click",function(){
 		document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active");
-		this.classList.add("resume-list__item--active")
+		document.querySelector(".resume-content--show").classList.remove("resume-content--show");
+		this.classList.add("resume-list__item--active");
+		let contentId = this.getAttribute("data-content-id");
+		document.querySelector(contentId).classList.add("resume-content--show");
+
 	})
 })
