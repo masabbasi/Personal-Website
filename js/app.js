3ac0525032a4861727a5d1a6bef61bff14232cfe
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-08-17 14:14:24 +0430

    Set Resume List - JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const mobileMenu = document.querySelector(".menu__mobile-icon");
 const Menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
+const resumeListItems = document.querySelectorAll(".resume-list__item");
 
 mobileMenu.addEventListener("click",function(){
 	mobileMenu.classList.toggle("menu__mobile-icon--open");
 	Menu.classList.toggle("menu--mobile-open");
 	cover.classList.toggle("cover--show");
+})
+
+resumeListItems.forEach(resumeListItem=>{
+	resumeListItem.addEventListener("click",function(){
+		document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active");
+		this.classList.add("resume-list__item--active")
+	})
 })
