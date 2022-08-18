d5a33f1bcc09fad79e2f227e49783c604ee15297
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-08-18 19:36:15 +0430

    Set Portfolio

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const mobileMenu = document.querySelector(".menu__mobile-icon");
 const Menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
 const resumeListItems = document.querySelectorAll(".resume-list__item");
+const portfolioListItems = document.querySelectorAll(".portfolio-list__item");
 
 mobileMenu.addEventListener("click",function(){
 	mobileMenu.classList.toggle("menu__mobile-icon--open");
@@ -9,13 +10,18 @@ mobileMenu.addEventListener("click",function(){
 	cover.classList.toggle("cover--show");
 })
 
-resumeListItems.forEach(resumeListItem=>{
-	resumeListItem.addEventListener("click",function(){
-		document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active");
-		document.querySelector(".resume-content--show").classList.remove("resume-content--show");
-		this.classList.add("resume-list__item--active");
-		let contentId = this.getAttribute("data-content-id");
-		document.querySelector(contentId).classList.add("resume-content--show");
+function navigationTabsInit (listItems,activeClass,showClass) {
+	listItems.forEach(listItem=>{
+		listItem.addEventListener("click",function(){
+			document.querySelector(`.${activeClass}`).classList.remove(activeClass);
+			document.querySelector(`.${showClass}`).classList.remove(showClass);
+			this.classList.add(activeClass);
+			let contentId = this.getAttribute("data-content-id");
+			document.querySelector(contentId).classList.add(showClass);
+	
+		})
+	})	
+}
 
-	})
-})
+navigationTabsInit (resumeListItems,"resume-list__item--active","resume-content--show");
+navigationTabsInit (portfolioListItems,"portfolio-list__item--active","portfolio-content--show");
