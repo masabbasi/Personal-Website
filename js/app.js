7d10a89497238f34e018be6890ad4ccea0494f31
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-08-24 12:47:06 +0430

    Set Scroll To Section

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const Menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
 const resumeListItems = document.querySelectorAll(".resume-list__item");
 const portfolioListItems = document.querySelectorAll(".portfolio-list__item");
+const menuItems = document.querySelectorAll (".menu__item");
 
 mobileMenu.addEventListener("click",function(){
 	mobileMenu.classList.toggle("menu__mobile-icon--open");
@@ -10,11 +11,15 @@ mobileMenu.addEventListener("click",function(){
 	cover.classList.toggle("cover--show");
 })
 
+function removeActiveClass (className) {
+	document.querySelector(`.${className}`).classList.remove(className);
+}
+
 function navigationTabsInit (listItems,activeClass,showClass) {
 	listItems.forEach(listItem=>{
 		listItem.addEventListener("click",function(){
-			document.querySelector(`.${activeClass}`).classList.remove(activeClass);
-			document.querySelector(`.${showClass}`).classList.remove(showClass);
+			removeActiveClass (activeClass)
+			removeActiveClass (showClass)
 			this.classList.add(activeClass);
 			let contentId = this.getAttribute("data-content-id");
 			document.querySelector(contentId).classList.add(showClass);
@@ -25,3 +30,17 @@ function navigationTabsInit (listItems,activeClass,showClass) {
 
 navigationTabsInit (resumeListItems,"resume-list__item--active","resume-content--show");
 navigationTabsInit (portfolioListItems,"portfolio-list__item--active","portfolio-content--show");
+
+menuItems.forEach(item => {
+	item.addEventListener("click",function (event){
+		event.preventDefault();
+		removeActiveClass ("menu__item--active");
+		item.classList.add("menu__item--active");
+		let sectionClass = item.getAttribute("data-section");
+		let sectionOffsetTop = document.querySelector(`.${sectionClass}`).offsetTop;
+		window.scrollTo({
+			top:sectionOffsetTop-130,
+			behavior:"smooth"
+		})
+	})
+})
