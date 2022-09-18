6a1723b1ec01d4c352a77881a0a1013fc8761c75
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-09-18 16:19:38 +0430

    Set Observer

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const cover = document.querySelector(".cover");
 const resumeListItems = document.querySelectorAll(".resume-list__item");
 const portfolioListItems = document.querySelectorAll(".portfolio-list__item");
 const menuItems = document.querySelectorAll (".menu__item");
+const sections = document.querySelectorAll("main>section")
 
 mobileMenu.addEventListener("click",function(){
 	mobileMenu.classList.toggle("menu__mobile-icon--open");
@@ -30,6 +31,24 @@ function navigationTabsInit (listItems,activeClass,showClass) {
 
 navigationTabsInit (resumeListItems,"resume-list__item--active","resume-content--show");
 navigationTabsInit (portfolioListItems,"portfolio-list__item--active","portfolio-content--show");
+
+const observer = new IntersectionObserver(observerHandler,{threshold:0.5});
+
+function observerHandler(allSection) {
+allSection.map(section=>{
+	let sectionClassName = section.target.className;
+	let sectionMenuItem = document.querySelector(`.menu__item[data-section=${sectionClassName}]`);
+	if(section.isIntersecting) {
+		sectionMenuItem.classList.add("menu__item--active");
+	} else {
+		sectionMenuItem.classList.remove("menu__item--active");
+	}
+})
+}
+
+sections.forEach(section=>{
+	observer.observe(section);
+})
 
 menuItems.forEach(item => {
 	item.addEventListener("click",function (event){
